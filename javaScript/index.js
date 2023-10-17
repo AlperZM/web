f548fe6a20f8d38a5132d35447b7619d19d2935f
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-10-17 13:47:19 +0300

    Update index.js

--- javaScript/index.js
+++ javaScript/index.js
@@ -4,3 +4,9 @@ let arr = [1,2,3,4,2,3,1,4,6,];
 let obj = {};
 arr.map(e=> obj[e] = (obj[e] || 0) +1);
 //obj = {1 : 2, 2 : 2, 3 : 3, 4 : 2, 6 : 1 }
+
+// sort by nth char
+const sortIt = (list, n) =>
+  list.split(`, `)
+    .sort((a, b) => a[n - 1].toLowerCase().localeCompare(b[n - 1].toLowerCase()) || a.localeCompare(b))
+    .join(`, `);
